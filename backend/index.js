85f02920f9ae4cc0ad866cc0d235dfa4916f1482
roshan <roshanprusty100@gmail.com>
2023-12-26 15:55:54 +0530

    MongoDB connection has been set

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import tourRoute from './routes/tour.js';
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 8000;
+
+//db connection
+mongoose.set('strictQuery', false);
+const connect = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB database connected');
+    }catch(err){
+        console.log('MongoDB connection failed');
+    }
+}
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use('/tours', tourRoute);
+
+app.listen(port, ()=>{
+    connect();
+    console.log('listening on port', port);
+});
---
+++ backend/routes/tour.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {createTour, deleteTour, getAllTour, getSingleTour, updateTour} from './../controllers/tourController.js';
+const router = express.Router();
+
+//create a new tour
+router.post('/', createTour);
+//update tour
+router.put('/:id', updateTour);
+//delete tour
+router.delete('/:id', deleteTour);
+//create a new tour
+router.get('/:id', getSingleTour);
+//create a new tour
+router.get('/', getAllTour);
+
+
+export default router;
